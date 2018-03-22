712b84b248e58378c016b65ce1dd3d0bd18ee93a
Lei YU <mine260309@gmail.com>
2018-03-22 16:57:32 +0800

    Add one-bit-adder to simulate one-bit adder
    It uses 4 q-bits, two for sum, one for result and one for carry.
    TODO: add one more q-bit for the carry to be added.

---
+++ one-bit-adder.py
@@ -0,0 +1,48 @@
+from qiskit import QuantumProgram
+
+Circuit = 'oneBitAdderCircuit'
+
+# Create the quantum program
+qp = QuantumProgram()
+
+# Creating registers
+n_qubits = 4
+qr = qp.create_quantum_register("qr", n_qubits)
+cr = qp.create_classical_register("cr", n_qubits)
+
+# One-bit adder circuit, where:
+# qr[2] = qr[0] + qr[1]
+# qr[3] = carry
+obc = qp.create_circuit(Circuit, [qr], [cr])
+
+# Prepare bits to add
+obc.h(qr[0])
+obc.h(qr[1])
+
+# qr[2] = 1 when qr0/1 has only one 1;
+#       = 0 when qr0/1 are both 0 or 1;
+obc.cx(qr[0], qr[2])
+obc.cx(qr[1], qr[2])
+
+# qr[3] = 1 when qr0/1 are both 1;
+#       = 0 otherwise;
+obc.ccx(qr[0], qr[1], qr[3])
+
+# Measure
+for i in range(0,n_qubits):
+    obc.measure(qr[i], cr[i])
+
+# Get qasm source
+source = qp.get_qasm(Circuit)
+print(source)
+
+# Compile and run
+backend = 'local_qasm_simulator'
+circuits = [Circuit]  # Group of circuits to execute
+
+qobj=qp.compile(circuits, backend) # Compile your program
+
+result = qp.run(qobj, wait=2, timeout=240)
+print(result)
+
+print(result.get_counts(Circuit))
